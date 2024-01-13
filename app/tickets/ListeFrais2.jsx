53fe8e63a2b5c1ddb7b6112e29ed7d52e6f6988e
marcamaudruz <marc.amaudruz@gmail.com>
2024-01-13 08:02:25 +0100

    svg

--- app/tickets/ListeFrais2.jsx
+++ app/tickets/ListeFrais2.jsx
@@ -8,11 +8,11 @@ export default async function ListeFrais2() {
   return (
     <>
       {/* Version laptop */}
-      <div className="hidden md:block relative flex flex-col w-full h-full text-gray-700 bg-white shadow-md rounded-xl bg-clip-border">
+      <div className="relative flex flex-col w-full h-full text-gray-700 bg-white shadow-md rounded-xl bg-clip-border">
         <table className="w-full text-left table-auto min-w-max">
           <thead>
             <tr>
-              <th className="p-4 border-b border-blue-gray-100 bg-blue-gray-50">
+              <th className="hidden md:block p-4 border-b border-blue-gray-100 bg-blue-gray-50">
                 <p className="block font-sans text-sm antialiased font-normal leading-none text-blue-gray-900 opacity-70">
                   Utilisateur
                 </p>
@@ -22,7 +22,7 @@ export default async function ListeFrais2() {
                   Titre
                 </p>
               </th>
-              <th className="p-4 border-b border-blue-gray-100 bg-blue-gray-50">
+              <th className="hidden md:block p-4 border-b border-blue-gray-100 bg-blue-gray-50">
                 <p className="block font-sans text-sm antialiased font-normal leading-none text-blue-gray-900 opacity-70">
                   Catégorie
                 </p>
@@ -37,7 +37,7 @@ export default async function ListeFrais2() {
                   Status
                 </p>
               </th>
-              <th className="p-4 border-b border-blue-gray-100 bg-blue-gray-50">
+              <th className="hidden md:block p-4 border-b border-blue-gray-100 bg-blue-gray-50">
                 <p className="block font-sans text-sm antialiased font-normal leading-none text-blue-gray-900 opacity-70"></p>
               </th>
             </tr>
@@ -46,17 +46,19 @@ export default async function ListeFrais2() {
           <tbody>
             {tickets?.map((ticket) => (
               <tr key={ticket.id}>
-                <td className="p-4 border-b border-blue-gray-50">
+                <td className="hidden md:block p-4 border-b border-blue-gray-50">
                   <p className="block font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
                     {ticket.user}
                   </p>
                 </td>
                 <td className="p-4 border-b border-blue-gray-50 ">
                   <p className="block font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
-                    {ticket.titre}
+                    <Link href={`/tickets/${ticket.id}`} className="hover:text-red-800">
+                      {ticket.titre.length > 15 ? `${ticket.titre.substring(0, 15)}...` : ticket.titre}
+                    </Link>
                   </p>
                 </td>
-                <td className="p-4 border-b border-blue-gray-50 ">
+                <td className="hidden md:block p-4 border-b border-blue-gray-50 ">
                   <p className="block font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
                     {ticket.categorie}
                   </p>
@@ -67,20 +69,20 @@ export default async function ListeFrais2() {
                   </p>
                 </td>
                 <td className="p-4 border-b border-blue-gray-50 ">
-                  <p className="block font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
+                  <div className={`pill ${ticket.status}`}>
                     {ticket.status}
-                  </p>
+                  </div>
                 </td>
-                <td className="p-4 border-b border-blue-gray-50">
-                  <Link
-                    href={`/tickets/${ticket.id}`}
-                    className="block font-sans text-sm antialiased font-medium leading-normal text-blue-gray-900"
-                  >
-                    View
-                  </Link>
-                  <form action={deleteTicket}>
+                <td className="hidden md:block p-4 border-b border-blue-gray-50">
+
+                  <form action={deleteTicket} className="pl-1">
+
                     <input type="hidden" name="id" value={ticket.id} />
-                    <SubmitButton name={"delete"} />
+                    {/* <SubmitButton name={"delete"} /> */}
+                    <svg dataSlot="icon" fill="none" strokeWidth={1.5} stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                      <path strokeLinecap="round" strokeLinejoin="round" d="m20.25 7.5-.625 10.632a2.25 2.25 0 0 1-2.247 2.118H6.622a2.25 2.25 0 0 1-2.247-2.118L3.75 7.5m6 4.125 2.25 2.25m0 0 2.25 2.25M12 13.875l2.25-2.25M12 13.875l-2.25 2.25M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z" />
+                    </svg>
+
                   </form>
                 </td>
               </tr>
--- app/tickets/[id]/page.jsx
+++ app/tickets/[id]/page.jsx
@@ -1,9 +1,11 @@
 import { fetchTicket } from "../../lib/data"
+import { deleteTicket } from "../../lib/actions";
+import { SubmitButton } from "../../components/Submit-button";
 
 export default async function TicketDetails({ params }) {
   const ticket = await fetchTicket(params.id)
-  const date = (ticket.createdAt).toLocaleString();
-  console.log(ticket.titre)
+  if (!ticket) {return <p>no ticket</p>}
+  // const date = (ticket.createdAt).toLocaleString();
   return (
     <main>
       <nav>
@@ -17,10 +19,14 @@ export default async function TicketDetails({ params }) {
         <p>Affaire: {ticket.affaire}</p>
         <p>Montant: {ticket.prix}</p>
         <p>Justificatif: {ticket.img}</p>
-        <p>Date de création: {date}</p>
+        {/* <p>Date de création: {date}</p> */}
         <div className={`pill ${ticket.status}`}>
           {ticket.status}
         </div>
+        <form action={deleteTicket} className="pl-1">
+          <input type="hidden" name="id" value={ticket.id} />
+          <SubmitButton name={"delete"} />
+        </form>
       </div>
     </main>
   )
